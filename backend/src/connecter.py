46b5a33a2d540237443bf561705a340fdac24eab
Fivetr <81582626+Fivetr@users.noreply.github.com>
2023-05-18 12:42:45 -0500

    Update connecter.py

--- backend/src/connecter.py
+++ backend/src/connecter.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    user="Li",
-    password="26285194",
-    host="cs4347.c3bw7ao2sqoy.us-west-2.rds.amazonaws.com",
-    database="Tutoring"
+    user="",
+    password="",
+    host="",
+    database=""
 )
 
 cursor = db.cursor()
